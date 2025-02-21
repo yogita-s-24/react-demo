c3908a745afe51406ac83b8b331951edf8569193
Yogita Shete <yogitashete24@gmail.com>
2025-02-21 12:23:37 +0530

    inc dec functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -38,14 +38,49 @@
 // };
 // export default App;
 
-import { UserProvider } from "./components/useContextHook/UserProfileUsingUseContext/UserContext";
-import UserProfile from "./components/useContextHook/UserProfileUsingUseContext/UserProfile";
+// import { UserProvider } from "./components/useContextHook/UserProfileUsingUseContext/UserContext";
+// import UserProfile from "./components/useContextHook/UserProfileUsingUseContext/UserProfile";
+
+// const App = () => {
+//   return (
+//     <UserProvider>
+//       <UserProfile />
+//     </UserProvider>
+//   );
+// };
+
+// export default App;
+
+import { useReducer } from "react";
+
+const initialState = { count: 0 };
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + 1 };
+
+    case "decrement":
+      return { ...state, count: state.count - 1 };
+
+    case "reset":
+      return { ...state, count: 0 };
+
+    default:
+      return state;
+  }
+};
 
 const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   return (
-    <UserProvider>
-      <UserProfile />
-    </UserProvider>
+    <div>
+      <button onClick={() => dispatch({ type: "increment" })}>+</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+      <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
+      <h1>Count : {state.count}</h1>
+    </div>
   );
 };
 
